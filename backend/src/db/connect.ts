f6ad6ba1948121f8657d5dab069d5600bb8efe42
aswinrajeev0 <aswinrajeev347@gmail.com>
2025-06-24 22:49:38 +0530

    env path added

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import path from "path"
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export const connectDB = async () => {
     try {
+        console.log(process.env.MONGO_URI)
         await mongoose.connect(process.env.MONGO_URI!);
         console.log(`MongoDB connected`);
     } catch (error) {
